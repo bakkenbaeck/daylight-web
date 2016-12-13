04f79a63c1baf74d916c2e040f238445a182fa62
August Skare <post@augustskare.no>
2016-12-13 22:13:48 +0100

    store user location in idb

--- javascript/app/index.js
+++ javascript/app/index.js
@@ -1,9 +1,59 @@
 import Horizon from './components/horizon';
+import Store from './store';
 
-const app = document.getElementById('app');
-const {latitude, longitude, city, country} = app.dataset;
-const location = {latitude, longitude};
-const now = new Date();
+class Sol {
+  constructor() {
 
-const horizon = new Horizon(now, location);
-horizon.init();
+    const app = document.getElementById('app');
+    const {latitude, longitude, city, country} = app.dataset;
+    const location = { 
+      location: {latitude, longitude},
+      city, country
+    };
+    const now = new Date();
+
+    Store.set('location', location);
+
+    const horizon = new Horizon(now, location.location);
+    horizon.init();
+
+    this.getUserLocation().then(position => {
+      if (location.city !== position.city) {
+        Store.set('location', position);
+        console.log('update ui with new location');
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+
+  }
+
+  getUserLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        location => resolve(this.reverseGeocode(location.coords).then(location => location)),
+        error => reject(error), 
+        { enableHighAccuracy: false }
+      );
+    });
+  }
+
+  reverseGeocode(location) {
+    return fetch(`http://nominatim.openstreetmap.org/reverse?format=json&lat=${location.latitude}&lon=${location.longitude}&addressdetails=1`)
+      .then(resp => resp.json())
+      .then(position => {
+        return {
+          location,
+          city: position.address.city,
+          country: position.address.country
+        }
+      });
+  }
+
+}
+
+new Sol();
+
+
+
+
---
+++ javascript/app/store.js
@@ -0,0 +1,23 @@
+import idb from 'idb';
+
+const dbPromise = idb.open('sol-store', 1, upgradeDB => {
+  upgradeDB.createObjectStore('sol');
+});
+ 
+const Store = {
+  get(key) {
+    return dbPromise.then(db => {
+      return db.transaction('sol')
+        .objectStore('sol').get(key);
+    });
+  },
+  set(key, val) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('sol', 'readwrite');
+      tx.objectStore('sol').put(val, key);
+      return tx.complete;
+    });
+  }
+};
+
+export default Store
