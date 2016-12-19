7f2c0e48d653e18714fbb6bd15b7e3c63f4e098c
Ezekiel Aquino <ezekielaquino@gmail.com>
2016-12-19 17:17:09 +0100

    Edits theme change script

--- javascript/landing/script.js
+++ javascript/landing/script.js
@@ -2,20 +2,23 @@ console.log('● Daylight by Bakken & Baæck.');
 
 const date = new Date();
 const hour = date.getHours();
+const min = date.getMinutes();
 let theme;
 
 // Just average hour values -> theme
-if (hour <= 8) {
+if (hour >= 7 && hour < 8) {
   theme = 'sunrise';
-} else if (hour <= 17) {
+} else if (hour >= 8 && hour < 17) {
   theme = 'daylight';
-} else if (hour <= 18) {
+} else if (hour >= 17 && hour < 18) {
   theme = 'sunset';
-} else if (hour <= 24) {
+} else if (hour >= 18 && hour < 24) {
   theme = 'night';
-} else {
+} else if (hour == 24 || hour < 7) {
   theme = 'twilight';
 }
+
+console.log(hour, theme, min);
 
 // Apply the theme to html
 document.documentElement.classList.add('theme-' + theme);
