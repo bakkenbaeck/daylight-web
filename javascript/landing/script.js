b47b19fc8827658972b62c181ff955cbb47b5ae9
Ezekiel Aquino <ezekielaquino@gmail.com>
2016-12-19 15:09:51 +0100

    [WIP] Initial commit. Icons to follow

---
+++ javascript/landing/script.js
@@ -0,0 +1,21 @@
+console.log('● Daylight by Bakken & Baæck.');
+
+const date = new Date();
+const hour = date.getHours();
+let theme;
+
+// Just average hour values -> theme
+if (hour <= 8) {
+  theme = 'sunrise';
+} else if (hour <= 17) {
+  theme = 'daylight';
+} else if (hour <= 18) {
+  theme = 'sunset';
+} else if (hour <= 24) {
+  theme = 'night';
+} else {
+  theme = 'twilight';
+}
+
+// Apply the theme to html
+document.documentElement.classList.add('theme-' + theme);
