897f31acf231465777c2f32108938ee17692aaf2
August Skare <post@augustskare.no>
2016-12-20 11:48:59 +0100

    added new sentences

--- javascript/utils/utils.js
+++ javascript/utils/utils.js
@@ -29,45 +29,55 @@ const generateSentence = (daylight, theme) => {
           `<span class="muted">We’ll have</span> <b>about a minute</b> <span class="muted">of extra light today. It’s upwards from here.</span>`,
         ],
       },
-      negative: [
-        `<span class="muted">The sun will be out</span> <b>${minutes} ${minuteString} less</b> <span class="muted">today. Keep your head up!</span>`,
-        `<b>${minutes} ${minuteString} less</b> <span class="muted">sunlight today, unfortunately. It’ll get better!</span>`,
-        `<span class="muted">Sadly, the day will be</span> <b>${minutes} ${minuteString} shorter</b><span class="muted">. Make the most out of it!</span>`,
-      ]
-    },
-    night: [
-      `<span class="muted">Get a good night’s sleep: tomorrow there’ll be</span> <b>${minutes} more ${minuteString}</b> <span class="muted">of sunlight.</span>`,
-      `<span class="muted">Lights out. Enjoy</span> <b>${minutes} more ${minuteString}</b> <span class="muted">of sunlight tomorrow!</span>`,
-      `<span class="muted">Bring out those pyjamas.</span> <b>${minutes} more ${minuteString}</b> <span class="muted">of light await tomorrow.</span>`,
-      `<span class="muted">The sun has set for today. Embrace those</span> <b>${minutes} ${minuteString}</b> <span class="muted">of extra daylight tomorrow.</span>`,
-      `<span class="muted">The sun has set. Soak up the extra vitamin D tomorrow!</span>`,
-    ],
-  } 
-  const nightFallback = `<span class="muted">The sun has set. Soak up the extra vitamin D tomorrow!</span>`;
-
-
-  const context = theme === 'night' ? 'tomorrow' : 'today';
-  const positive = daylight[context].positive
-  let sentenceArray = [];
-
-  if (theme === 'night') {
-    if (positive && minutes >= 1) {
-      sentenceArray = sentences.night;
-    } else {
-      return nightFallback;
-    }
-  } else {
-    if (positive) {
-      if (minutes >= 1) {
-        sentenceArray = sentences.day.positive.minutes;
-      } else {
-        sentenceArray = sentences.day.positive.seconds;
+      negative: {
+        minutes: [
+          `<span class="muted">The sun will be out</span> <b>${minutes} ${minuteString} less</b> <span class="muted">today. Keep your head up!</span>`,
+          `<b>${minutes} ${minuteString} less</b> <span class="muted">sunlight today, unfortunately. It’ll get better!</span>`,
+          `<span class="muted">Sadly, the day will be</span> <b>${minutes} ${minuteString} shorter</b><span class="muted">. Make the most out of it!</span>`,
+        ],
+        seconds: [
+          `<span class="muted">Unfortunately, the day is a little bit shorter today. Make the most out of it!</span>`,
+          `<span class="muted">Sadly, today is a tiny bit shorter than yesterday. Enjoy it while it lasts!</span>`,
+          `<span class="muted">Today is shorter than yesterday. But fear not, brighter times ahead!</span>`,
+        ],
       }
-    } else {
-      sentenceArray = sentences.day.negative;
+    },
+    night: {
+      positive: {
+        minutes: [
+          `<span class="muted">Get a good night’s sleep: tomorrow there’ll be</span> <b>${minutes} more ${minuteString}</b> <span class="muted">of sunlight.</span>`,
+          `<span class="muted">Lights out. Enjoy</span> <b>${minutes} more ${minuteString}</b> <span class="muted">of sunlight tomorrow!</span>`,
+          `<span class="muted">Bring out those pyjamas.</span> <b>${minutes} more ${minuteString}</b> <span class="muted">of light await tomorrow.</span>`,
+          `<span class="muted">The sun has set for today. Embrace those</span> <b>${minutes} ${minuteString}</b> <span class="muted">of extra daylight tomorrow.</span>`,
+          `<span class="muted">The sun has set. Soak up the extra vitamin D tomorrow!</span>`,
+        ],
+        seconds: [
+          `<span class="muted">Get a good night’s sleep: tomorrow there’ll be more sunlight for you.</span>`,
+          `<span class="muted">Bring out those pyjamas. More daylight awaits tomorrow!</span>`,
+          `<span class="muted">The sun has set. Soak up the extra vitamin D tomorrow!</span>`,
+        ],
+      },
+      negative: {
+        minutes: [
+          `<span class="muted">Unfortunately, tomorrow will be</span> <b>${minutes} ${minuteString}</b> <span class="muted">shorter than today. Make the most out of it!</span>`,
+          `<span class="muted">Sadly, tomorrow will be</span> <b>${minutes} ${minuteString}</b> <span class="muted">shorter than today. Enjoy it while it lasts!</span>`,
+          `<span class="muted">Tomorrow will be</span> <b>${minutes} ${minuteString}</b> <span class="muted">shorter than today. But fear not, brighter times ahead!</span>`,
+        ],
+        seconds: [
+          `<span class="muted">Unfortunately, tomorrow will be a little bit shorter than today. Make the most out of it!</span>`,
+          `<span class="muted">Sadly, tomorrow will be a tiny bit shorter than today. Enjoy it while it lasts!</span>`,
+          `<span class="muted">Tomorrow will be shorter than today. But fear not, brighter times ahead!</span>`,
+        ],
+      }
     }
-  }
+  } 
 
+
+  const phase = theme === 'night' ? 'night' : 'day';
+  const feeling = daylight[theme === 'night' ? 'tomorrow' : 'today'].positive ? 'positive' : 'negative';
+  const sunLength = minutes >= 1 ? 'minutes' : 'seconds';
+
+  let sentenceArray = sentences[phase][feeling][sunLength];
   return sentenceArray[Math.floor(Math.random() * sentenceArray.length)];
 }
 
