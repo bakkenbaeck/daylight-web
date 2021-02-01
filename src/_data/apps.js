dcccd9f968b3902566c455669e0534da14d6a811
August Skare <post@augustskare.no>
2021-02-01 14:38:05 +0100

    Move app data in data file

---
+++ src/_data/apps.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    title: "Web app",
+    cta: "Open app",
+    image: "/assets/icon-web.png",
+    url: "/app",
+    external: false,
+  },
+  {
+    title: "iPhone app",
+    cta: "Get the app",
+    image: "/assets/icon-ios.png",
+    url: "https://itunes.apple.com/us/app/daylight-for-iphone/id1187597286?ls=1&mt=8",
+    external: true,
+  },
+  {
+    title: "Android app",
+    cta: "Get the app",
+    image: "/assets/icon-android.png",
+    url: "https://play.google.com/store/apps/details?id=com.bakkenbaeck.sol",
+    external: true,
+  },
+];
