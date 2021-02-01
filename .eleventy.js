fc6d550114ae66389db1ba086bf0911e6c51c6ca
August Skare <post@augustskare.no>
2021-02-01 15:05:02 +0100

    Inline javascript for landing page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,19 @@
+const { minify } = require("terser");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+    code,
+    callback
+  ) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error("Terser error: ", err);
+      callback(null, code);
+    }
+  });
+
   eleventyConfig.addPassthroughCopy({"src/static": "."});
 
   return {
